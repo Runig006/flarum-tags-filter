ce4a45ac0261905558accdf1d8d59d66368f9f74
Alejandro_Osendi <alejandro.osendi@doubledot.es>
2022-04-11 05:06:13 +0200

    First commit

---
+++ js/src/forum/components/TagsDropdown.tsx
@@ -0,0 +1,62 @@
+import Component from 'flarum/common/Component';
+import Button from 'flarum/common/components/Button';
+import Dropdown from 'flarum/common/components/Dropdown';
+import app from 'flarum/forum/app';
+
+import type Mithril from 'mithril';
+
+import Tag from 'flarum/tags/models/Tag';
+
+interface IAttrs {}
+
+interface IState {
+  data: Tag[];
+  selected: Array;
+}
+
+export default class TagsDropdown extends Component<IAttrs, IState> {
+  oninit(vnode: Mithril.Vnode<IAttrs, this>): void {
+    super.oninit(vnode);
+    this.state = {
+      data: app.store.all('tags').filter(tag => !tag.parent()),
+      selected: sessionStorage.getItem('tags_filters') ? JSON.parse(sessionStorage.getItem('tags_filters')) : [],
+    };
+  }
+
+  view() {
+    let content = this.state.data?.map((tag) => (
+      <Button  className={`TagFilter-item Button ${this.state.selected.indexOf(tag.slug()) >= 0 ? "active" : ""}`}
+        onclick={() => {
+          this.handleTagChange(tag);
+        }}
+      >
+        {tag.name()}
+      </Button>
+    ));
+
+    return (
+      <Dropdown
+        buttonClassName="Button"
+        label="Tags"
+        updateOnClose
+      >
+       {content}
+      </Dropdown>
+    );
+  }
+
+  handleTagChange(tag: Tag | null) {
+    let params = app.search.params();
+    let index = this.state.selected.indexOf(tag.slug());
+    if(index >= 0){
+      this.state.selected.splice(index,1);
+    }else{
+      this.state.selected.push(tag.slug());
+    }
+    sessionStorage.setItem('tags_filters', JSON.stringify(this.state.selected));
+
+    params.tags = this.state.selected.join(',');
+    console.log(params);
+    app.discussions.refreshParams(params, 2);
+  }
+}
