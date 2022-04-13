c7be3e641358ef7da3da1ce87d307a9da0a50fe1
Alejandro_Osendi <alejandro.osendi@doubledot.es>
2022-04-14 00:57:40 +0200

    Added local storage, primary colors

--- extend.php
+++ extend.php
@@ -16,5 +16,6 @@ use Flarum\Extend;
 return [
     (new Extend\Frontend('forum'))
         ->js(__DIR__ . '/js/dist/forum.js')
+        ->css(__DIR__.'/less/forum.less')
 
 ];
--- js/src/forum/components/TagsDropdown.tsx
+++ js/src/forum/components/TagsDropdown.tsx
@@ -7,7 +7,7 @@ import type Mithril from 'mithril';
 
 import Tag from 'flarum/tags/models/Tag';
 
-interface IAttrs {}
+interface IAttrs { }
 
 interface IState {
   data: Tag[];
@@ -17,15 +17,26 @@ interface IState {
 export default class TagsDropdown extends Component<IAttrs, IState> {
   oninit(vnode: Mithril.Vnode<IAttrs, this>): void {
     super.oninit(vnode);
+    let selected = [];
+    let saved = localStorage.getItem('spy-tags-filters');
+    if (saved) {
+      selected = JSON.parse(saved);
+    } else {
+      selected = [];
+    }
     this.state = {
-      data: app.store.all('tags').filter(tag => !tag.parent()),
-      selected: app.search.params().tags ? app.search.params().tags.split(',') : [],
+      data: app.store.all('tags').filter(tag => !tag.parent()).sort(
+        function(a,b){
+          return a.position() - b.position();
+        }
+      ),
+      selected: selected
     };
   }
 
   view() {
     let content = this.state.data?.map((tag) => (
-      <Button  className={`TagFilter-item Button ${this.state.selected.indexOf(tag.slug()) >= 0 ? "active" : ""}`}
+      <Button className={`TagFilter-item Button ${this.state.selected.indexOf(tag.slug()) >= 0 ? "active" : ""}`}
         onclick={() => {
           this.handleTagChange(tag);
         }}
@@ -40,7 +51,7 @@ export default class TagsDropdown extends Component<IAttrs, IState> {
         label="Tags"
         updateOnClose
       >
-       {content}
+        {content}
       </Dropdown>
     );
   }
@@ -48,11 +59,12 @@ export default class TagsDropdown extends Component<IAttrs, IState> {
   handleTagChange(tag: Tag | null) {
     let params = app.search.params();
     let index = this.state.selected.indexOf(tag.slug());
-    if(index >= 0){
-      this.state.selected.splice(index,1);
-    }else{
+    if (index >= 0) {
+      this.state.selected.splice(index, 1);
+    } else {
       this.state.selected.push(tag.slug());
     }
+    localStorage.setItem('spy-tags-filters', JSON.stringify(this.state.selected));
     params.tags = this.state.selected.join(',');
     app.discussions.refreshParams(params, 2);
   }
