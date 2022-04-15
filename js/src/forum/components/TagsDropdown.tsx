56d96bb08647312eb695389d72af96a42ad65af5
Alejandro_Osendi <alejandro.osendi@doubledot.es>
2022-04-15 16:40:21 +0200

    All tags

--- js/src/forum/components/TagsDropdown.tsx
+++ js/src/forum/components/TagsDropdown.tsx
@@ -25,11 +25,7 @@ export default class TagsDropdown extends Component<IAttrs, IState> {
       selected = [];
     }
     this.state = {
-      data: app.store.all('tags').filter(tag => !tag.parent()).sort(
-        function(a,b){
-          return a.position() - b.position();
-        }
-      ),
+      data: sortTags(app.store.all('tags')),
       selected: selected
     };
   }
@@ -68,4 +64,48 @@ export default class TagsDropdown extends Component<IAttrs, IState> {
     params.tags = this.state.selected.join(',');
     app.discussions.refreshParams(params, 2);
   }
+}
+
+
+//Is from Common Sorts Tags, but the fucker doesnt export it
+function sortTags(tags) {
+  return tags.slice(0).sort((a, b) => {
+    const aPos = a.position();
+    const bPos = b.position();
+
+    // If they're both secondary tags, sort them by their discussions count,
+    // descending.
+    if (aPos === null && bPos === null)
+      return b.discussionCount() - a.discussionCount();
+
+    // If just one is a secondary tag, then the primary tag should
+    // come first.
+    if (bPos === null) return -1;
+    if (aPos === null) return 1;
+
+    // If we've made it this far, we know they're both primary tags. So we'll
+    // need to see if they have parents.
+    const aParent = a.parent();
+    const bParent = b.parent();
+
+    // If they both have the same parent, then their positions are local,
+    // so we can compare them directly.
+    if (aParent === bParent) return aPos - bPos;
+
+    // If they are both child tags, then we will compare the positions of their
+    // parents.
+    else if (aParent && bParent)
+      return aParent.position() - bParent.position();
+
+    // If we are comparing a child tag with its parent, then we let the parent
+    // come first. If we are comparing an unrelated parent/child, then we
+    // compare both of the parents.
+    else if (aParent)
+      return aParent === b ? 1 : aParent.position() - bPos;
+
+    else if (bParent)
+      return bParent === a ? -1 : aPos - bParent.position();
+
+    return 0;
+  });
 }
